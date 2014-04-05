2e34532e92fc58231405336c82574a5e0c1be590
KE Claytor <ke.claytor@gmail.com>
2014-04-05 13:12:57 -0400

    Basic image thresholding. updated to image grab

--- imagecapture.py
+++ imagecapture.py
@@ -3,15 +3,7 @@ from cv2 import *
 
 # initialize the camera
 def take_image(cindex = 1)
-    cindex = 1
     cam = VideoCapture(cindex)   # 0 -> index of camera
     s, img = cam.read()
-    imwrite("filename.jpg",img) #save image
-
-#if s:    # frame captured without any errors
-#    namedWindow("cam-test",CV_WINDOW_AUTOSIZE)
-#    imshow("cam-test",img)
-#    waitKey(0)
-#    destroyWindow("cam-test")
-#    imwrite("filename.jpg",img) #save image
+    return img
 
--- processimage.py
+++ processimage.py
@@ -1,5 +1,16 @@
 # Process the image we acquired
+from cv2 import *
+import numpy as np
+from matplotlib import pyplot as plt
+from util_image import show_image_window
 
+# Read the image we saved
+img = imread('testnew.jpg',0)
 # Segment into blocks
+# Threshold to pick the blocks from the background
+ret,threshold = threshold(img,127,255,THRESH_BINARY)
+show_image_window(threshold)
+
+# Now isolate the blocks
 
 # Create a block object for each block and fill it in as much as we can
