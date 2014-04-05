0cf57e957bef757675ce3fc2d8478c04574d37f6
KE Claytor <ke.claytor@gmail.com>
2014-04-05 13:10:21 -0400

    useful image definitions.

---
+++ util_image.py
@@ -0,0 +1,8 @@
+# Uesful image utilities
+
+# Show image in new window
+def show_image_window(img):
+    namedWindow("cam-test",CV_WINDOW_AUTOSIZE)
+    imshow("cam-test",img)
+    waitKey(0)
+    destroyWindow("cam-test")
